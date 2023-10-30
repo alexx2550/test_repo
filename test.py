e00162633701009bceaa0a59cefdf779d882b00f
Alex <alexander110607@gmail.com>
2023-10-30 23:11:08 +0400

    Add function which reverse the list element.

--- test.py
+++ test.py
@@ -2,9 +2,19 @@ def first_func(a, b):
 
     return a ** b
 
-first_func(5, 8)
+# first_func(5, 8)
+
 
 def second_func(c, d):
     return c + d
 
-second_func(10, 963)
+# second_func(10, 963)
+
+
+def reverse_list(ls: list):
+    for i in ls:
+        rev_ls = i[::-1]
+        return rev_ls
+
+ls = ['Alexnader Babayan']
+print(reverse_list(ls))
